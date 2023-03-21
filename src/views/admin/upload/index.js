58c2a91cfa1b51be92bb51f5aa7aa9023bf361f1
Tsantaniaina Rakotonjanahary <tsantaniainarakotonjanahary@gmail.com>
2023-03-21 03:57:25 +0300

    go

---
+++ src/views/admin/upload/index.js
@@ -0,0 +1,31 @@
+// ** React Importsnippet
+import { Fragment, useEffect } from 'react'
+
+// ** Reactstrap Imports
+import { Row, Col, CardBody, CardText , CardTitle } from 'reactstrap'
+
+// ** Third Party Components
+import prism from 'prismjs'
+
+
+// ** Custom Components
+import Card from '@components/card-snippet'
+import Breadcrumbs from '@components/breadcrumbs'
+
+
+const PageOne = () => {
+
+
+  return (
+    <Row>
+      <Col md='6' sm='12'>
+        <Card title={"go"} >
+          <h1>upload</h1>
+        </Card>
+      </Col>
+     
+    </Row>
+  )
+}
+
+export default PageOne;
