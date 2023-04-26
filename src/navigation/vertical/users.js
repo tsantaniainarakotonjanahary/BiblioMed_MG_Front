10011db34bf10028322e656f29f69241ae0be990
Tsantaniaina Rakotonjanahary <69953669+tsantaniainarakotonjanahary@users.noreply.github.com>
2023-04-26 13:11:06 +0300

    go

--- src/navigation/vertical/users.js
+++ src/navigation/vertical/users.js
@@ -1,11 +1,12 @@
-import { Menu, Circle, EyeOff, Folder, LifeBuoy, Shield , Home , Plus  , Loader , Share2 , BookOpen , Globe} from 'react-feather'
+import { Menu, Circle, EyeOff, Folder, LifeBuoy, Shield , Home , Plus  , Loader , Share2 , BookOpen , Globe , Check } from 'react-feather'
 
 export default [
   { header: 'Users' },
-  { id: 'user-home', title: 'Accuei', icon: <Home size={20} />, action: 'manage', resource: 'for-user', navLink: '/user/accueil' },
+  { id: 'user-home', title: 'Accuei et Dashboard ', icon: <Home size={20} />, action: 'manage', resource: 'for-user', navLink: '/user/accueil' },
   { id: 'user-upload', title: 'Ajouter nouveau', icon: <Plus size={20} />, action: 'manage', resource: 'for-user', navLink: '/user/upload' },
   { id: 'pending-file', title: 'En attente', icon: <Loader size={20} />, action: 'manage', resource: 'for-user', navLink: '/user/pending-file' },
   { id: 'file-by-entity', title: 'Partagé', icon: <Share2 size={20} />, action: 'manage', resource: 'for-user', navLink: '/user/file-by-entity' },
-  { id: 'user-valid', title: 'Fichier validé', icon: <BookOpen size={20} />, action: 'manage', resource: 'for-user', navLink: '/user/valid' },
+  { id: 'user-valid', title: 'Fichier validé', icon: <Check size={20} />, action: 'manage', resource: 'for-user', navLink: '/user/valid' },
   { id: 'user-public', title: 'Fichier public', icon: <Globe size={20} />, action: 'manage', resource: 'for-user', navLink: '/user/public' },
+  
 ]
