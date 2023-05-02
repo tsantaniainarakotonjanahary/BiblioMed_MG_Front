84139bee0f1a9b6f249aa256b7d468c808bd3dbf
Tsantaniaina Rakotonjanahary <69953669+tsantaniainarakotonjanahary@users.noreply.github.com>
2023-05-02 04:23:30 +0300

    go

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import NodeGlobalsPolyfillPlugin from '@esbuild-plugins/node-globals-polyfill'
 
 export default () => {
   return defineConfig({
+    base: process.env.NODE_ENV === 'production' ? '/' : '/',
     plugins: [react()],
     define: {
       global: 'globalThis'
