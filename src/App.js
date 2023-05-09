81ab029bf7d6bfaf815349131beed5333dc0688e
Tsantaniaina Rakotonjanahary <69953669+tsantaniainarakotonjanahary@users.noreply.github.com>
2023-05-09 20:06:25 +0300

    go

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React, { Suspense ,  useEffect } from 'react'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 
-
-
-
-// ** Router Import
 import Router from './router/Router'
 
 const App = () => {
 
-  useEffect(() => {
-    AOS.init({
-      duration: 1000,
-      easing: 'ease',
-    });
-  }, []);
-
-  
   return (
     <Suspense fallback={null}>
       <Router />
